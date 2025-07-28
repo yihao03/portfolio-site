9b3e462bd64eebc0a85fcf5f55e22e993325b94a
yihao <yihao039@gmail.com>
2025-07-28 17:43:15 +0800

    removed cloudflare from plugins to fix direct path

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [cloudflare(), react(), tailwindcss()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
